e319e6879065d9d659cc43dc4ead5edc861a66e8
RAbeyrathna <rahal@res-network.com>
2023-12-11 19:49:34 +1100

    Change exercise database display function to clear until user input received

--- exercise_list.py
+++ exercise_list.py
@@ -1,4 +1,5 @@
 from colored import Fore, Back, Style
+from functions import clear_console
 import csv
 
 exercises_file_path = "exercises.csv"
@@ -16,7 +17,10 @@ except FileNotFoundError:
 def el_menu():
     user_selection = ""
     while user_selection != 5:
-        print(f"{Style.BOLD}{Fore.yellow} -- Exercise Database Menu --\n")
+        print(f"{Style.BOLD}{Fore.CYAN} -- Exercise Database Menu --\n")
+        print(
+            f"{Style.BOLD}{Fore.YELLOW}Please select from the following options:{Style.reset}\n"
+        )
         print(f"{Fore.blue}[ 1 ] Display all exercises in database")
         print(f"[ 2 ] Edit an exercise from the database")
         print(f"[ 3 ] Add a new exercise to the database")
@@ -31,46 +35,57 @@ def el_menu():
             if user_selection < 1 or user_selection > 5:
                 raise ValueError
         except ValueError:
+            clear_console()
             print(
-                f"\n{Fore.red}Please enter a valid number between 1 and 5:{Style.reset}\n"
+                f"{Fore.red}Please enter a valid number between 1 and 5:{Style.reset}\n"
             )
 
         if user_selection == 1:
-            print(
-                f"\n{Fore.blue}Displaying all exercises in Exercise Database:{Style.reset}\n"
-            )
+            clear_console()
             el_display()
         elif user_selection == 2:
+            clear_console()
             print(f"\n{Fore.blue}Choose an exercise to edit:{Style.reset}\n")
         elif user_selection == 3:
+            clear_console()
             print(f"\n{Fore.blue}Adding new exercise to the database...{Style.reset}\n")
         elif user_selection == 4:
+            clear_console()
             print(
                 f"\n{Fore.blue}Choose an exercise to delete from the database:{Style.reset}\n"
             )
 
     print(f"{Fore.red}\nExiting Workout Templates Menu...\n{Style.reset}")
+    clear_console()
 
 
 def el_display():
-    with open(exercises_file_path, "r") as exercises_file:
-        csv_reader = csv.reader(exercises_file)
-        header = next(csv_reader)
-        print(
-            f"{Fore.CYAN}{'Exercise':<25}{'Default Weight':<15}{'Default Reps':<15}{'Default Sets':<15}{Style.RESET}"
-        )
-        for row in csv_reader:
-            (
-                exercise_name,
-                default_weight,
-                default_reps,
-                default_sets,
-            ) = row
+    clear_console()
+    print(f"{Fore.blue}Displaying all exercises in Exercise Database:{Style.reset}\n")
+    close_menu = False
+    while not close_menu:
+        with open(exercises_file_path, "r") as exercises_file:
+            csv_reader = csv.reader(exercises_file)
+            header = next(csv_reader)
             print(
-                f"{Fore.YELLOW}{exercise_name:<25}{default_weight:<15}{default_reps:<15}{default_sets:<15}{Style.RESET}"
+                f"{Fore.CYAN}{'Exercise':<25}{'Default Weight':<15}{'Default Reps':<15}{'Default Sets':<15}{Style.RESET}"
             )
-        print(f"\n{Fore.CYAN}[ END OF LIST ]{Style.reset}\n")
+            for row in csv_reader:
+                (
+                    exercise_name,
+                    default_weight,
+                    default_reps,
+                    default_sets,
+                ) = row
+                print(
+                    f"{Fore.YELLOW}{exercise_name:<25}{default_weight:<15}{default_reps:<15}{default_sets:<15}{Style.RESET}"
+                )
+            print(f"\n{Fore.CYAN}[ END OF LIST ]{Style.reset}\n")
+        input("Press enter when you would like to return to the exericise list menu:\n")
+        close_menu = True
+
     exercises_file.close()
+    clear_console()
 
 
 def el_edit():
