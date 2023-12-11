258dd26b9488a3a8bcebd67ac5d52ec05c046fc8
RAbeyrathna <rahal@res-network.com>
2023-12-11 19:48:33 +1100

    Clean up main menu module

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+from functions import clear_console
 from colored import Fore, Back, Style
 from workout_templates import wt_menu
 from workout_entry import we_menu
 from exercise_list import el_menu
 from previous_workouts import pw_menu
 
+
+clear_console()
+
 print(
-    f"{Style.BOLD}{Fore.YELLOW}\nWelcome the the Workout Tracker application. Please select from the following options:{Style.reset}\n"
+    f"{Style.BOLD}{Fore.BLUE}Welcome the the Workout Tracker application!{Style.reset}\n"
 )
 
 
 def display_menu():
     user_selection = ""
     while user_selection != 5:
-        print(f"{Style.BOLD}{Fore.yellow} -- Main Menu --\n")
+        print(f"{Style.BOLD}{Fore.CYAN} -- Main Menu --\n")
+        print(
+            f"{Style.BOLD}{Fore.YELLOW}Please select from the following options:{Style.reset}\n"
+        )
         print(f"{Fore.cyan}[ 1 ] Enter Workout Entry")
         print(f"[ 2 ] Enter Workout Templates Menu")
         print(f"[ 3 ] View Exercise List Database")
@@ -27,27 +34,21 @@ def display_menu():
             if user_selection < 1 or user_selection > 5:
                 raise ValueError
         except ValueError:
+            clear_console()
             print(
-                f"\n{Fore.red}Please enter a valid number between 1 and 5:{Style.reset}\n"
+                f"{Fore.red}Please enter a valid number between 1 and 5:{Style.reset}\n"
             )
-
         if user_selection == 1:
-            print(
-                f"\n{Fore.blue}Entering 'Enter Workout Entry' Function...{Style.reset}\n"
-            )
+            clear_console()
             we_menu()
         elif user_selection == 2:
-            print(f"\n{Fore.blue}Entering 'Workout Templates' Menu...{Style.reset}\n")
+            clear_console()
             wt_menu()
         elif user_selection == 3:
-            print(
-                f"\n{Fore.blue}Entering 'View Exercise List Database' Function...{Style.reset}\n"
-            )
+            clear_console()
             el_menu()
         elif user_selection == 4:
-            print(
-                f"\n{Fore.blue}Entering 'View Previous Workout Entries' Function...{Style.reset}\n"
-            )
+            clear_console()
             pw_menu()
     print(f"{Fore.red}\nQuitting the application...{Style.reset}\n")
 
