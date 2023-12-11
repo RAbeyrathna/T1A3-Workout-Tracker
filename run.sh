dc1b3f7f4ace2fc8851e4ee9b71d2b65273327ae
RAbeyrathna <rahal@res-network.com>
2023-12-11 14:38:29 +1100

    Create bash script to run application

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Add command to check if Python is installed - Print error message to user to install python 
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install colorama
+python3 main.py
