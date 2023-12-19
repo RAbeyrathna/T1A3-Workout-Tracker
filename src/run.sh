ca603679fef94c6b32c9225397ec5ea6cd10beda
RAbeyrathna <rahal@res-network.com>
2023-12-19 14:29:34 +1100

    Update bash script with requirements.txt and more verbose error

--- src/run.sh
+++ src/run.sh
@@ -4,10 +4,10 @@ if python3 --version &>/dev/null; then
     python3 -m venv .venv
     source .venv/bin/activate
 
-    pip3 install colored
-    pip3 install prettytable
+    pip3 install -r ./requirements.txt
 
     python3 main.py
 else
-    echo "Python was not detected. Please install Python before running this application"
+    echo "Error: Python was not detected. This application used Python to run. Please install Python 3 before running this application"
+    exit 1
 fi
